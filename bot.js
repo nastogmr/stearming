21fe82cdb4e439bcf9760213f6cc24d32e77907d
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-08-12 15:19:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Welcome Bro ${client.user.tag}!`);
 });
-var prefix = '#'
+var prefix = 'nasto+'
 
 client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
@@ -14,40 +14,40 @@ client.on('message', message => {
   if (message.author.id !== "608388769107935252") return;
 
   
-  if (message.content.startsWith(prefix + 'setwatch')) {
+  if (message.content.startsWith(prefix + 'wat')) {
   client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
-    message.channel.sendMessage(`**~~__احلى حالة من ناستو: ${argresult}__~~**`)
+    message.channel.sendMessage(`** By NaSto <3 : ${argresult}**`)
 } 
 
  
-  if (message.content.startsWith(prefix + 'setlis')) {
+  if (message.content.startsWith(prefix + 'lis')) {
   client.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('test' + argresult);
-    message.channel.sendMessage(`**~~__احلى حالة من ناستو: ${argresult}__~~**`)
+    message.channel.sendMessage(`** By NaSto <3 : ${argresult}**`)
 } 
 
 
 if (message.content.startsWith(prefix + 'setname')) {
   client.user.setUsername(argresult).then
-      message.channel.sendMessage(`**~~__احلى حالة من ناستو: ${argresult}__~~**`)
+      message.channel.sendMessage(`** By NaSto <3 : ${argresult}**`)
   return message.reply("You Can change the username 2 times per hour");
 } 
 
 if (message.content.startsWith(prefix + 'setavatar')) {
   client.user.setAvatar(argresult);
-   message.channel.sendMessage(`**~~__احلى حالة من ناستو: ${argresult}__~~**`);
+   message.channel.sendMessage(`** By NaSto <3 : ${argresult}**`);
 }
 
-if (message.content.startsWith(prefix + 'setT')) {
+if (message.content.startsWith(prefix + 'st')) {
   client.user.setGame(argresult, "https://www.twitch.tv/peery13");
      console.log('test' + argresult);
-    message.channel.sendMessage(`**~~__احلى حالة من ناستو: ${argresult}__~~**`)
+    message.channel.sendMessage(`** By NaSto <3 : ${argresult}**`)
 } 
-if (message.content.startsWith(prefix + 'setgame')) {
+if (message.content.startsWith(prefix + 'ply')) {
   client.user.setGame(argresult);
      console.log('test' + argresult);
-    message.channel.sendMessage(`**~~__احلى حالة من ناستو: ${argresult}__~~**`)
+    message.channel.sendMessage(`** By NaSto <3 : ${argresult}**`)
 } 
 
 
